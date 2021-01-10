67681529fb90696405721d641b0d6099b487619f
Stoninis <76572732+Stoninis@users.noreply.github.com>
2021-01-10 16:52:15 +0200

    Update veidrodzioProgramaa.py
    Pakeista elementų vieta. Pagal orus galima rodyti atitinkamą nuotrauką

--- veidrodzioProgramaa.py
+++ veidrodzioProgramaa.py
@@ -3,37 +3,46 @@ from tkinter import *
 import requests
 import serial
 import time
+import PIL
+from PIL import Image, ImageTk
 
+# narsykle
 #Puslapis, is kurio traukiama informacija ir mano narsykles agentas
-nuoroda = "https://www.accuweather.com/en/lt/silute/228452/current-weather/228452"
+nuoroda = "https://www.accuweather.com/lt/lt/silute/228452/current-weather/228452"
 agentas = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
+atsakymas = requests.get(nuoroda, headers=agentas) 
+soup = BeautifulSoup(atsakymas.content, "html.parser")
 
+# Kintamieji
 res = [1920, 1080] # ekrano raiska
+veikimoAtstumas = 50 # kokiu atstumu isijungia veidrodis (cm)
+atnaujinimas = 500 # po kiek ms is naujo pradedama funkcija
 
 # Kintamieji elementu formatavimui
 LangasSpalva = "black"
 LangasVisas = True #ar langas uzima visa ekrana
 
 LaikoFormatas = "%H:%M:%S" #"%H:%M:%S"
-LaikoVieta = [res[0]/4, res[1]/4]
+LaikoVieta = [res[0]/20, res[1]/10]
 LaikrodisSpalva = ["white", "black"] #indeksas 0 - fg| 1 - bg
 LaikrodisDydis = 44
-LaikrodisSriftas = "Courier"
+LaikrodisSriftas = "calibri light"
 
-tempDabarVieta = [res[0]/4, res[1]/4+50]
-tempDabarSpalva = ["white", "black"] #indeksas 0 - fg| 1 - bg
-tempDabarDydis = 12
-tempDabarSriftas = "Courier"
+tempIrOrasDabarVieta = [res[0]*0.86, res[1]*0.1+130]
+tempIrOrasDabarSpalva = ["white", "black"] #indeksas 0 - fg| 1 - bg
+tempIrOrasDabarDydis = 15
+tempIrOrasDabarSriftas = "calibri light"
 
-veikimoAtstumas = 50 # kokiu atstumu isijungia veidrodis (cm)
-atnaujinimas = 500 # po kiek ms is naujo pradedama funkcija
+pavVieta = [ res[0]*0.87, res[1]*0.1]
+pavBG = "black"
 
-#USB sukurimas, parametrai
+# USB sukurimas, parametrai
 usb = serial.Serial() 
 usb.baudrate = 9600
 usb.port = "COM5"
 usb.open()
 
+# Labels
 # Lango sukurimas
 langas = Tk()
 langas.attributes('-fullscreen', LangasVisas)
@@ -41,33 +50,53 @@ langas.configure(bg = LangasSpalva)
 
 # Laikrodzio teksto sukurimas
 laikrodis = Label(fg = LaikrodisSpalva[0], bg = LaikrodisSpalva[1])
-laikrodis.place( x = LaikoVieta[0] , y = LaikoVieta[1], anchor = "center")
+laikrodis.place( x = LaikoVieta[0] , y = LaikoVieta[1], anchor = "nw")
 laikrodis.configure(font=(LaikrodisSriftas, LaikrodisDydis))
 
-# Dabartines temperaturos teksto sukurimas
-tempDabar = Label(fg = tempDabarSpalva[0], bg = tempDabarSpalva[1])
-tempDabar.place( x = tempDabarVieta[0] , y = tempDabarVieta[1], anchor = "center")
-tempDabar.configure(font=(tempDabarSriftas, tempDabarDydis))
+# Dabartines temperaturos ir oroteksto sukurimas
+tempIrOrasDabar = Label(fg = tempIrOrasDabarSpalva[0], bg = tempIrOrasDabarSpalva[1])
+tempIrOrasDabar.place( x = tempIrOrasDabarVieta[0] , y = tempIrOrasDabarVieta[1], anchor = "center")
+tempIrOrasDabar.configure(font=(tempIrOrasDabarSriftas, tempIrOrasDabarDydis))
 
-def tempDabarYra():
-    #dabartines temperaturos gavimas
-    atsakymas = requests.get(nuoroda, headers=agentas) 
-    soup = BeautifulSoup(atsakymas.content, "html.parser")
-    temptemp = soup.find(class_ = "display-temp").get_text()
-    tempDabar.configure(text = "Šiluma Šilutėje dabar: " + temptemp)
+#paveikslelis
+img = ImageTk.PhotoImage(Image.open("C:\\Users\\Stoninis\\Desktop\\Oras ir Laikas\\oras clipart\\empty.png"))
+panel = Label(image = img, bg = pavBG)
+panel.place( x = pavVieta[0], y = pavVieta[1], anchor = "n")
+
+# funkcijos    
+def tempIrOrasDabarYra():
+    pranesimas = soup.find(class_ = "phrase").get_text() + " " + soup.find(class_ = "display-temp").get_text()
+    tempIrOrasDabar.configure(text = pranesimas)
+
+def ikeltiPNG():
+    img = ImageTk.PhotoImage(Image.open("C:\\Users\\Stoninis\\Desktop\\Oras ir Laikas\\oras clipart\\"+soup.find(class_ = "phrase").get_text()+".png"))
+    panel.configure(image = img)
+    panel.image = img
+
+
+def visiIjungti():
+    laikrodis.configure(text = time.strftime(LaikoFormatas, time.localtime()))
+    tempIrOrasDabarYra()
+    ikeltiPNG()
+
+def visiTusti():
+    laikrodis.configure(text = " ")
+    tempIrOrasDabar.configure(text = " ")
+
+    img = ImageTk.PhotoImage(Image.open("C:\\Users\\Stoninis\\Desktop\\Oras ir Laikas\\oras clipart\\empty.png"))
+    panel.configure(image = img)
+    panel.image = img
 
 def ekranas():
     serialIn = int(usb.readline().decode("utf-8")) #nuskaitom atstumo is arduino #bitai is arduino verciami i sveikuosius skaicius
+
     if serialIn <= veikimoAtstumas: #jei atstumas iki jutiklio didesnis nei vaikmoAtstumas
-        laikrodis.configure(text = time.strftime(LaikoFormatas, time.localtime()))
-        tempDabarYra()
-                
+       visiIjungti()
+        
     elif serialIn > veikimoAtstumas: #jei atstumas iki jutiklio didesnis nei vaikmoAtstumas
-        laikrodis.configure(text = " ")
-        tempDabar.configure(text = " ")
-    
+        visiTusti()
+        
     langas.after(atnaujinimas, ekranas)
 
 ekranas()
-
 langas.mainloop()
